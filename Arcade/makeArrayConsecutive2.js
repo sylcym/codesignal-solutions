ae322eea501e94404836c9c29bfa8305b9c70f16
sylcym <prusak.syl@gmail.com>
2021-05-14 21:48:53 +0200

    initial commit

---
+++ Arcade/makeArrayConsecutive2.js
@@ -0,0 +1,8 @@
+function makeArrayConsecutive2(statues) {
+  let min = Math.min(...statues);
+  let max = Math.max(...statues);
+  let newStatues = [];
+  for( let i = min ;i<= max ;i++){
+      newStatues.push(i);
+  }
+  return newStatues.length -statues.length
