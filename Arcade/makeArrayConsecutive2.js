048aeb028f594e896e34e0002598c294ec1fcad0
sylcym <prusak.syl@gmail.com>
2021-05-14 21:51:19 +0200

    js update

--- Arcade/makeArrayConsecutive2.js
+++ Arcade/makeArrayConsecutive2.js
@@ -2,7 +2,10 @@ function makeArrayConsecutive2(statues) {
   let min = Math.min(...statues);
   let max = Math.max(...statues);
   let newStatues = [];
-  for( let i = min ;i<= max ;i++){
-      newStatues.push(i);
+
+  for (let i = min; i <= max; i++) {
+    newStatues.push(i);
   }
-  return newStatues.length -statues.length
+  
+  return newStatues.length - statues.length;
+}
