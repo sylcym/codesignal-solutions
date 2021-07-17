014cf79012f59b2320e055fd13f1f3c557375e25
sylcym <prusak.syl@gmail.com>
2021-07-17 14:49:53 +0200

    initial commit

---
+++ Arcade/the core/96-christmasTree.js
@@ -0,0 +1,27 @@
+function christmasTree(levelNum, levelHeight) {
+  let maxWidth = 5 + (levelHeight + levelNum - 2) * 2;
+  
+  let tree = [
+    " ".repeat((maxWidth - 1) / 2) + "*",
+    " ".repeat((maxWidth - 1) / 2) + "*",
+    " ".repeat((maxWidth - 3) / 2) + "***"
+  ];
+
+  for(let i= 0; i < levelNum; i++){
+      for(let j= 0; j < levelHeight; j++){
+        tree.push(
+            ' '.repeat((levelHeight - 1 - j )+ (levelNum - i - 1)) 
+            + '*'.repeat(5 + 2 * (j + i))
+        )
+      }
+  }
+  
+  for (let i = 0; i < levelNum; i++) {
+    tree.push(
+        ' '.repeat((maxWidth - 1) / 2 - Math.floor(levelHeight / 2)) 
+        + '*'.repeat(levelHeight % 2 === 0 ? levelHeight + 1 : levelHeight)
+    );
+  }
+  
+  return tree;
+}
